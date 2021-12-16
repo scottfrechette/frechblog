ba63ccaa81bafdc74151055082ca79862a7740d0
Scott Frechette <scottfrechette@gmail.com>
2021-12-15 22:46:56 -0600

    Update ghostwriter theme

--- themes/ghostwriter/webpack.config.js
+++ themes/ghostwriter/webpack.config.js
@@ -1,42 +1,39 @@
 const path = require('path');
-const perfectionist = require('perfectionist');
-const discardComments = require('postcss-discard-comments');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   entry: {
-    styles: path.join(__dirname, 'static', 'styles'),
+    site: path.join(__dirname, 'static', 'styles', 'site'),
+    syntax: path.join(__dirname, 'static', 'styles', 'syntax'),
   },
   resolve: {
     modules: [
       'node_modules'
     ],
-    extensions: ['.js', '.scss'],
+    extensions: ['.scss'],
   },
   output: {
     path: path.join(__dirname, 'static', 'dist'),
-    filename: '[name].css'
   },
   plugins: [
-    new ExtractTextPlugin('[name].css'),
-    new OptimizeCssAssetsPlugin({
-      cssProcessor: discardComments,
-      canPrint: false
+    new FixStyleOnlyEntriesPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
     }),
-    new OptimizeCssAssetsPlugin({
-      cssProcessor: perfectionist,
-      cssProcessorOptions: {
-        format: 'compact'
-      },
-      canPrint: false
-    })
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' })
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       }
     ]
   }
